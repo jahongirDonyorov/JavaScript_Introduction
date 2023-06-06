fc338579c836243954a368440026873d525c553e
Jahon <donyorovjahon@gmail.com>
2023-06-06 19:22:54 +0100

    Vesual Studio Code Extension

--- dars_2.js
+++ dars_2.js
@@ -28,3 +28,21 @@
 
 // -=-=-=-=-=-=-=-=- extensions -=-=-=-=-=-=-=-=-=-=-
 // Vesual studio code da codlarni osonroq yozish uchin extension dan foydalanamiz
+
+// 1. All Autocomplete 
+//        Bu bizga codimizni tuldirib beradi, yani bror codening birinchi harfini yozsak qolganini uzi chiqarib beradi
+// 2. Auto Close Tag 
+//        Bu bizga automatizke taglarimizni yopib beradi. Ochuluvchi tagni yozsak qolganini uzi chiqarib beradi
+// 3. Auto Rename Tag
+//        Bu bizga 2 talik taglarni uzgartirishda ishlatiladi. ochiluvchi tag nomini uzgartirsak <h1> yopuluvchisi </h1> automatik ravishda uzgaradi
+// 4. Auto Complete Tag 
+//        Bu bizga codeni complete qilib beradi 
+// 5. Bracket Pair Colorizer 2
+//        Bu bizga Js kodimizda qavslar kupayb ketganda qaysi qavs qaysiga tegishli ekanligini kursatadi
+//        !!! hozirda bu extension ishlamaydi because Vs Code ni uziga qushilgan sittings bulimiga @id:editor.bracketPairColorization.enabled @id:editor.guides.bracketPairs - shuni yozib true qilsak ishlaydi
+// 6. Code Runner
+//        Bu bizga JS kodimizni Run qilib (tremenalda ishlatib) beradi 
+// 7. CSS Peek
+//        Bu bizga Html da turib uzimizga kerakli class ga css da qanaqa stelies berilganini kursatadi
+//        Kurish uchun kerakli classni ustiga meshkani olib borib turish kerak
+// 8. 
