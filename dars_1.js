854710c3b2accfec483a7baff251a7a8d26f2a82
Jahon <donyorovjahon@gmail.com>
2023-06-06 16:36:43 +0100

    My Js Manual

---
+++ dars_1.js
@@ -0,0 +1,70 @@
+// -#-#-#-#-#-#-#-#- Kirish
+
+// -=-=-=-=-=-=- belgi -=-=-=-=-=-=-=-
+
+// -#-#-# - eng asosiy narsalar
+// -=-=- - mavzu nomi
+// --=--=-- - qushimcha malumotlar
+// !_!_ - Eslatma ( yodlash qilish shart bulgan narsalar )
+// #_#_#_#_# - darsning (mavzuning) oxiri (tugashi)
+
+// #_#_#_#_#_#_#
+
+// -=-=-=-=-=-=-=- GitHub -=-=-=-=-=-=-=-
+// 1. github.com dan ruyhattan utish
+// 2. github new repositories
+// 3. Git install
+// 4. --=--=-- Project Terminal --=--=--
+// 5. git init - projectni git ga ulaydi
+// 6. git config --global/local user.name '.....' - birinchi marotaba kirilyotgan bulsa global qilganimiz yaxshi
+// 7. git config --global/local email.name '.....'
+// 8. git status - malumotlarni chiqarib beradi github ga quyilgan quyilmaganligini bilib olsak buladi
+// 9. git add -A/file nomi - gitHub ga projectni ulaydi
+// 10. git commit -m '....' - camentaria yozadi
+// 11. github ning uzidagi codeni termenalda yozamiz
+
+// --=--=-- Shu bilan bizning project github ga joylanadi --=--=--
+// bu usul faqat new repositories qilganimizdagina ishlatamiz
+
+// usha file ni uzgartirgandan (quwimcha quwganda) sung github ga joylash
+
+// 1. git add -a/file nomi
+// 2. git commit - yangi kamentariya
+// 3. git push - github ga yuklaydi (junatadi)
+
+// GitHub extension - bu bizga vs code ni uzida codlarni githubga qachon yuklaganimizni kursatadi
+
+// git log - bu bizga githubga kim qachon nimani yuborganligini kursatadi (Full info)
+
+// -=-=-=-=-= colne qilish -=-=-=-=-=-=
+// bu narsa bizga jamoa blan ishlayotganda juda kerak buladi
+
+// 1. git clone .... - ... urniga biz cone qilmoqchi bulgan folderni githubdan link kini olib quyamiz
+// clone qilingandan kiyin usha file ichiga cirib olamiz 2 ta file dan bittasi uzgargandan sung boshqattan clone qilinmaydi
+// 2. git pull - qilinadi bu bizga githubda uzgargan code blan bir hil qilib beradi
+
+// !_!_!_!_!_!_! shu narsalarni bir marotaba tuliq qaytarisng !_!_!_!_!_!_!_!
+// !_!_!_!_!_!_! Har kun bir 1 marotaba GitHub ga narsa (push) joylash shart !_!_!_!_!_!_!_!_!
+// bu narsadan maqsad ishga kirayotganda githab da faolligimizniham kurishi mumkun agar biz har kun narsa joylasak github da faol dasturchi bulamiz
+// kuproq narsa joylashga harakat qiling
+
+// --=--=--=--=-- Homework --=--=--=--=--=--
+// 1. Darsni tuliq takrorlash
+// 2. Jamoa blan html css dan tashkil topgan projectni GitHub da almashib qilish
+// 3. bitta file ni 3 4 martta clone qilish
+
+// -=-=-=-=-=-=-=- Linkiden -=-=-=-=-=-=-=-
+// linkiden ishtimoiy tarmoq hisblanib dasturchilarga ish topishda, yamgi malumotlarni bilishda juda kerak platforma hisoblanadi
+// 1. accaunt ochib kirib olamaiz
+// 2. kerakli malumotlarni tuldiramiz
+
+// !_!_!_!_!_!_! linkiden da faol bulish harkun harhil post dasturlashga oid narsalarni joylash !_!_!_!_!_!_!_!_!
+// Youtubedan linkiden haqida kuproq malumot kurish
+
+// -=-=-=-=-=-=-=- EdClub -=-=-=-=-=-=-=-
+// bu bizga klaviatoriaga qaramasdan tez yozishimiz uchun kerak buladi
+// Dasturchiga tez yozish eng kerakli narsa hisoblanadi bu blan biz ancha vahtimizni tejab qolamiz va ishlarimizni tezroq tugatishimiz mumkun
+// 1. Ruyhattan utib kirib olamiz
+
+// !_!_!_!_!_!_! Har kun EdClub ga kirish minemum 30 daqiqa !_!_!_!_!_!_!_!_!
+
---
+++ dars_2.js
@@ -0,0 +1,30 @@
+// -#-#-#-#-#-#-#-#- Kirish 
+
+// -=-=-=-=-=-=- belgilar -=-=-=-=-=-=-=- 
+
+// -#-#-# - eng asosiy narsalar
+// -=-=-= - mavzu nomi 
+// --=--=-- - qushimcha malumotlar
+// !_!_ - Eslatma ( yodlash qilish shart bulgan narsalar )
+// #_#_#_#_# - darsning (mavzuning) oxiri (tugashi)
+
+
+// #_#_#_#_#_#_#
+
+
+// -#-#-#-#-#-#-#-#-#- Vesual Studio Code Settings -#-#-#-#-#-#-#-#-#-#-#-#-
+
+// -=-=-=-=-=-=-=- Vesual Studio Code Access
+
+// -=-=-=-=-= Search
+// bu blan biz codimizddagi uzgartirmoqchi bulgan barcha birhil suzlarni topib bittada uzgartirsak buladi
+
+// "Search" bulimiga uzgartirmoqchi bulgan suzimizni 
+// "Replace" bulimiga qaysi suzga uzgarishi kerak bulsa ushani yozamiz 
+
+// -=-=-=-=-= Seurce control
+// bu git repositories blan ishlashda kerak buladi
+// bu blan gitHub codeni hech qanday termenalsiz oson va tez joylashimiz va kuzatib borishimiz mumkum
+
+// -=-=-=-=-=-=-=-=- extensions -=-=-=-=-=-=-=-=-=-=-
+// Vesual studio code da codlarni osonroq yozish uchin extension dan foydalanamiz
