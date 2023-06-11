7750ec2191e79fa373c0faee6d6854871eca3323
Jahon <donyorovjahon@gmail.com>
2023-06-11 19:10:58 +0100

    DATA TYPE Convirsiond

---
+++ dars_4.js
@@ -0,0 +1,16 @@
+// -#-#-#-#-#-#-#-#- Kirish
+
+// -=-=-=-=-=-=- belgilar -=-=-=-=-=-=-=-
+
+// -#-#-# - eng asosiy narsalar
+// -=-=-= - mavzu nomi
+// --=--=-- - qushimcha malumotlar
+// !_!_ - Eslatma ( yodlash qilish shart bulgan narsalar )
+// #_#_#_#_# - darsning (mavzuning) oxiri (tugashi)
+
+// #_#_#_#_#_#_#
+
+// -=-=-=-=-=-=- TYpe Convirsiond - data type larni uzgartirish
+
+
+// number blan string qushilganda b
