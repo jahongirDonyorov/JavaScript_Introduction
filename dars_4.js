47b22056d5556e50798c7bd728ce4728d78697ee
Jahon <donyorovjahon@gmail.com>
2023-06-14 20:11:32 +0100

    Comparision

--- dars_4.js
+++ dars_4.js
@@ -12,7 +12,7 @@
 
 // -=-=-=-=-=-=- Type Convirsiond - data type larni uzgartirish
 
-// String > Number
+// // String > Number
 
 // // Number
 // let a = 12,
@@ -25,7 +25,11 @@
 // let c = "12"; // bu string son yozilgan bulsaham u '' ichida bulganligi uchun string hisoblanadi
 // console.log(a + c); // 1212 buladi javob chunke JS number blan stringni qushganda faqatgina bir birini yoziga obkeb birlashtirib quyadi
 // // bunaqa hatolikni tug'irlash uchun  string uzgaruvchi oldiga arifmitik amal quwiwimiz kerak
-// console.log(a + +c); // 24
+// console.log(a * +c); // 24
+
+// console.log(typeof(c)) // string
+// console.log(typeof(-c)) // number
+
 // // 1 - plus 2 ta variable ni bir birga qushish uchun 2 - plus (+c) stringni numberga uzgartirish uchun
 // // string oldidan arifmirik amal qushsak u numberga aylanadi
 
@@ -58,10 +62,15 @@
 // // Bu elementimiz NaN yoke NaN emasligini bilib beradi
 // console.log(isNaN(a)); // false chunke a uzgaruvchisi NaN emas
 
-// // NaN bulishi uchun number va string qushilib ketgan element bulishi kerak
+// // let v = 12,
+// //    f= 'dd';
+// // let h = v + +f;
+// // console.log(isNaN(h))
+
+// // // NaN bulishi uchun number va string qushilib ketgan element bulishi kerak
 
 // let n = '1h';
-// console.log(isNaN(n))// true chunke bu Not a Number
+// console.log(isNaN(+n))// true chunke bu Not a Number
 
 // #_#_#_#_#_#_#_#_#_#_#_#_#_#
 
@@ -97,15 +106,14 @@
 // // 5. Remainder %,
 // // 6. Braces (),
 // // 7. Exponentiation **,
-// // 8. Equality =, -=, ===,
-// // 9. Obrivations +=, ==, /=, *=,
+// // 8. Equality =, ==, ===,
+// // 9. Obrivations +=, -=, /=, *=,
 // // 10. Increment ++,
 // // 11. Decrement --,
 
 // // _!_!_!_!_!_!_ Buyirdaham arifmetik amallar matimatikadagidek ketma ketlikda buladi
 
 // // misol: 1+4-(2-5)*4/2 - 1. ()ichi; 2. *; 3. /; 4. +; 5. -; - ketmaketligi buyicha ishlanadi
-
 
 // //     1. Qushish (+)
 // // holatga qarab xar xil vazifani bajarishi mumkun
@@ -122,29 +130,23 @@
 
 // // +b buyirdagi + arifmetik amallarimizga halaqt bermaydi bu faqat b (string) ni number qilib beradi
 
-
 // //     2. Ayirish (-)
-// console.log(a - +b); // -1
-
+// console.log(a - -b); // -1
 
 // //     3. Kupaytirish (*)
 // console.log(a * +b); // 2
 
-
 // //     4. bulish (/)
 // console.log(a / +b); // 0.5
-
 
 // //     5. Qoldiq (%)
 // // son bitta qiymatga bulinganda ortib qolgan qismi qoldiq diyiladi
 // console.log(101 % 10); // buyirda qoldiq 1
 
-
 // //     6. Qavs ()
 // // buni ham oddiy matimatikada ishlatgandek arifmetik amallar bajarishda ishlatamiz
 // console.log(1 + 4 - ((2 - 5) * 4) / 2); // 11
 // // har doim birinchi qavs ichi ishlanadi
-
 
 // //     7. Daraja **,
 // // bu bizga sonning darajsini chiqarib beradi
@@ -155,39 +157,37 @@
 // console.log(2 ** 4)//  2,  2 * 2 = 4, 4 * 2 = 8, 8 * 2 = 16
 // console.log(40 ** 6) // value: 4096000000
 
-
 // //     8. Equality =, ==, ===,
-// // tengliklar 
+// // tengliklar
 
 // // 1. =
-// // (=) bittalik tenglik. bundan bror bir narsaga boshqa bir narsani tenglashtirish uchun foydalanamiz 
+// // (=) bittalik tenglik. bundan bror bir narsaga boshqa bir narsani tenglashtirish uchun foydalanamiz
 // let tit = 2; // tit nomli variableni 2 ga tenglashtiryapdi
-// tit = 4; // tit nomli qiymati 2 ga teng variableni qiymatini 4 ga tenglashtiryapdi 
+// tit = 4; // tit nomli qiymati 2 ga teng variableni qiymatini 4 ga tenglashtiryapdi
 
 // // 2. ==
 // // (==) ikkitalik tenglik bu 2 ta narsani bir biriga taqqoslashda ishlatiladi
 // console.log(2 == '2') // true
-// // bu faqat songa qarayapdi data types ni hisobga olmayapdi 2 == '2' bulsaham tug'ri buladi chunke ikkalasiham 2 
-// // bu unchalik chuqur tekshirmaydi 1 ta narsasi tug'ri kelsaham true chiqaradi 
+// // bu faqat songa qarayapdi data types ni hisobga olmayapdi 2 == '2' bulsaham tug'ri buladi chunke ikkalasiham 2
+// // bu unchalik chuqur tekshirmaydi 1 ta narsasi tug'ri kelsaham true chiqaradi
 
 // // 3. ===
 // // (===) uchtalik tenglik bu 2 ta narsani bir biriga taqqoslashda ishlatiladi
 // // faqat bu har bir narsaga ahamiyat beradi yani bir biriga 100% tug'ri bulishi kerak
-// console.log(2 === '2') // false chunke bittasining ata type pi string bittasiniki number 
-// console.log(2 === 2) // true ikkalasiham number bulganligi uchun 
+// console.log(2 === '2') // false chunke bittasining data type pi string bittasiniki number
+// console.log(2 === 2) // true ikkalasiham number bulganligi uchun
 // // bu chuqur tekshiradi hamma narsasi 100% tug'ri bulsa true chiqaradi
 
-
 // //     9 . Obrivations +=, -=, /=, *=,
-// // 1. += 
+// // 1. +=
 // // uzini qiymatiga kerakli sonni qushib uziga tenglashtiradi
 
 // // x += 10, (x = x + 10) - shuni qisqartmasi
 // // 10 += 20, (10 = 10 + 20) = 30
 
 // let tex = 10;
-// // 10   10 + 20 =  12
-//    tex = tex + 2; 
+// // 10   10 + 2 =  12
+//    tex = tex + 2;
 //    console.log(tex); // value: 12
 
 // // buning qisqartirilgan usuliham mavjud
@@ -195,7 +195,6 @@
 //     tex += 2; // value: 14
 
 // // Qolganlariham (-=, /=, *=) huddi shundek faqat belgilariga qarab ayirib (-), bulib (/), kupaytiradi(*)
-
 
 // //     10. Increment ++,
 // // Bu ++ songa 1 qushib beradi
@@ -207,23 +206,23 @@
 // console.log(c); // value: 2
 
 // // console.log ni uzidaham c++ qilsak buladi
-
-// console.log(c++); // value: 1
-// // chunke ++ c dan kiyin turganligi uchun console 1 - c ni qiymatini chiqaradi kiyin ucha qiymatga 1 ni qushadi
-// console.log(c); // value: 2
+// // holat 1
+// console.log(c++); // value: 2 + 1 = 3
+// // chunke ++ c dan kiyin turganligi uchun console 1 - c ni qiymatini chiqaradi kiyin usha qiymatga 1 ni qushadi
+// console.log(c++); // value: 3 + 1 =4
 // // chunke bundan oldin c qiymati (1) ni chiqarib unga 1 qushgan edi kiyin c 2 ga teng bulagan
 
-// console.log(c++); // value: ???
-// console.log(c); // value: ???
+// // console.log(c++); // value: ???
+// // console.log(c); // value: ???
 
-// /*** !_!_!_!
+// // /*** !_!_!_!
 
-// Eslatma: Single srit
+// // Eslatma: Single srit
 
-// Single srit ning holatiam manashunda har bir amalni 1ta 1ta birn ketn bajaryapdi
-// agar mount srit bulganida ikkala amalni birdan bajarib 1 - holatni uzida 2 chiqargan bulardi 
+// // Single srit ning holatiham manashunda har bir amalni 1ta 1ta birn ketn bajaryapdi
+// // agar mount srit bulganida ikkala amalni birdan bajarib 1 - holatni uzida 3 chiqargan bulardi
 
-// !_!_!_! ***/
+// // !_!_!_! ***/
 
 // // ++ ni oldinga chiqarsakham buladi
 // let d = 1;
@@ -234,12 +233,11 @@
 // // misol:
 // let x = 19;
 
-// console.log(x++); // 1. value: ???
-// ++x;
-// console.log(++x); // 2. value: ???
-// x++;
-// console.log(x++); // 3. value: ???
-
+// console.log(x++); // 1. value: ??? / 19 // 19, 19 + 1 = 20
+// ++x; // 21
+// console.log(++x); // 2. value: ??? / 22
+// x++; // 22 + 1 = 23
+// console.log(x++); // 3. value: ??? 23, 23 + 1
 
 // //     11. Decrement --, (bu ham huddi ++ dek faqat ayiradi)
 // // Bu -- sondan 1 ayiradi
@@ -258,3 +256,80 @@
 // let inc = 1;
 // console.log(--inc); // value: 0
 
+
+// #_#_#_#_#_#_#_#_#_#_#_#_#_#
+
+
+// -=-=-=-=-=-=-=-=-=- Comparision 
+// <,>, ==, ===, <=, >=,!, malumotlarni compare qilish doim true yoki false qiymat qaytaradi
+
+// 1. < > - Katta kichik belgisi, elementlarni taqqoslashda ishlatamiz
+
+let a = 10,
+b = 13,
+c = 10;
+
+console.log(a > b) // false, chunke buyirda a katta b dan deyapdi lekn a, b dan kichik 
+console.log(a < b) // true, chunke a , b dan kichik 
+// tug'ri bulsa true notug'ri bulsa false chiqadi 
+console.log(c < b) // false 
+// 2. =, ==, ===
+
+// 2.1 - = verablega yangi qiymat tenglashtirish uchun ishlatiladi
+let f = '11'; // f ga '11' qiymat tenglashtirildi 
+
+// 2.2 - == taqqoslash 2 ta narsani bir biriga taqqoslaydi
+// faqat qiymatni uzini tekshiradi
+console.log(c == f) // true
+
+// 2.3 - === Qattiy tenglik
+// data type niham tekshiradi 
+console.log(c === f) // false
+
+// 3. <=, >= - Katta yoke teng , kichik yoke teng.
+// >< belgilar bu holatda har doim = ni chap tomonida bulishi kerak =<, >= - bu holatlar notug'ri 
+// <=, >= - true
+
+// ikkalasidan bittasi tug'ri kelsa true chiqaradi
+console.log(c <= a) // true  -  katta 
+console.log(c <= b) // true  -  teng 
+
+console.log(c >= a) // true - teng
+console.log(c >= b) // false - kichkina 
+
+// --=--=--=-- ASCII TABLE
+// har bir harf, belgilarning raqami. tartib raqamiga qarab katta kichigligini aniqlasak buladi. raqam qancha katta bulsa darajasi oshadi
+// A 65, a 97 = bu a bundan A katta
+console.log('A' > 'a') // false. ASCII TABLE buyicha kichik a birinchi keladi 
+console.log('a' > 'A') // true.  
+
+console.log('abcb' > 'abca') // true har bir harfni bitta bitta tekshirib boradi 
+
+// boshidan hisoblaydi boshidagi bittasi kichik bulib qolsa qolgan hammasi kichik hisoblanadi undan kiyin nechchi qancha kelishidan qatte nazar
+console.log('567' < '299999') // false chunke boshida 5 dan 2 kichkina qolganini ahamiyatga olmay ketadi boshida bittasi kichik bulsa 
+
+// 4. !=, !== - Teng emas
+// bularniham taqqoslashda ishlatamiz faqat bular teng emasligini taqqoslaydi
+// teng bulmasa true teng bulsa false chiqadi 
+
+// 4.1 - !=  Teng emas
+// bu faqat qiymatni tekshiradi
+
+console.log(1 != '1') // false chunke 1 blan 1 teng
+console.log(1 != 2) // true chunke bu ikkalasi bir biriga teng emas 
+
+// 4.2 - !==  Qattiy teng emas
+// bu data type lariniham tekshiradi 
+
+console.log(1 !== '1') // true chunke 2 xil data typeda ikkalasi bu data type niham tekshirar edi ikkalasi bir biriga teng emas 
+console.log(1 !== 1) // false ikkalasi bir biriga teng 
+
+// 5. !... - teskarisini chiqaradi 
+// true va false oldiga ! quysak usha narsani teskarisini chiqaradi 
+console.log(!true) // false teskarisini chiqardi
+console.log(!false) // true teskarisini chiqardi
+
+console.log(!1) // false 1 true bulganligi uchun uni teskarisini chiqarayapdi
+console.log(!'1') // bundaham shunday 
+
+console.log(!0) // true 0 false bulganligi uchun uni teskarisi (true) ni chiqarayapdi  
