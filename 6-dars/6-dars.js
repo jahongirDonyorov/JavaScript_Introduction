8dcbae2aaed6b476264405392d55ddd63a0a0917
Jahon <donyorovjahon@gmail.com>
2023-06-24 15:25:38 +0100

    For loops Basics

--- 6-dars/6-dars.js
+++ 6-dars/6-dars.js
@@ -65,3 +65,101 @@
 // // !_!_! break ni olib tashlab yozsak xatolik kursatmaydi lekn hamma case larni uqib hammasini javobini chiqaradi
 
 // _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+
+
+// // =-=-=-=-=-=-=-=-=-=-= For Loops Basics
+
+// // for lops uziga 3 ta porametr oladi
+// // for(boshlanish; manzil; yurish){}
+
+// // 1   2    3     4   5
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// // 1. kalit suz
+// // 2. uzgaruvchi yaratib qiymat berib olamz. boshlanish nuqtasi hisoblanadi
+// // 3. shart. qayargacha borishini kursatsak buladi
+// // 4. qanday borishi i++ bitta bitta quwib borsin degani
+// // 5. code
+
+// // 3. shart har doim true yoke false qiymatni kutadi true bulsa ishlaydi false bulsa tuxltaydi
+
+// // 2 uzgaruvchini tashqarida yozib chaqirib quysakham buladi
+// let c = 1;
+// for (c; i <= 10; i++) {
+//   console.log(c);
+//   console.log("hello word"); // buham 10 martta chiqadi
+// }
+
+// // kode tuliq ishlab bulgandan kiyingina qiymat birga oshadi bu shu holda takrorlanadi
+
+// // bu holatda  i kichik bulsa 10 dan i ga birni qushib bor deyilyapdi
+// // i=1, 10 dan kichik bulganligi uchun code birmartta ishlab 1 ni quwadi 1 + 1 = 2
+// // i=2, 10 dan kichik code yana bir tuliq ishlab i ga 1 ni quwaadi 2 + 1 = 3
+// // 10 ga teng bulguncha code ishlayveradi qachonke 10 ga teng bulganida ishlashdan tuxtaydid
+
+// // mislo:
+// // masalan biz 1 dan 10 gacha bulgan sonlarni console da chiqarmoqchimiz
+// // console.log(1)
+// // console.log(2)
+// // console.log(3)
+// // console.log(4)
+// // console.log(5)
+// // console.log(6)
+// // console.log(7)
+// // console.log(8)
+// // console.log(9)
+// // console.log(10)
+
+// // lekn biz buni for orqali bitta consolda chiqarishimiz mumkun
+
+// //   1     2     3
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+// // bu holatda ancha vahniham tejab qolamz
+
+// // 1. boshlanish nuqtasi nechhcidan boshlanishini shu yirda yozishimiz mumkun
+// // 2. manzil nichchi gacha bulishini shuyerda yozamiz
+// // 3. qanday borishi i++ bitta bitta quwib boradi buni uzimiz uzgartirishimiz mumkun i = i + 2,3,3..
+
+// for (i = 1; i <= 10; i = i + 2) {
+//   console.log(i);
+// }
+// // bu bizga 2 tadan quwib valueni chiqarib beradi
+
+// for(; i < 12; i++){
+//   console.log(i)
+// }
+// // bu holatda birinchi qiymatni olib quysakham ishlayveradi 
+// // lekn i++ ni olsak shart hechqachon false qiymatga utmaydi va davomiy cheksiz ishlayveradi 
+
+// for(i=1 ;i < 12; ){
+//   console.log(i)
+// }
+
+// // i++ ni { ichiga yozib quysak ham buladi }
+
+// for(i=1 ;i < 12; ){
+//   console.log(i)
+//   i++
+// }
+
+
+// -=-=-=-=-=-=- var vs let
+// shu yirda var blan let ni farqini yaxshiroq tushinib olsak buladi
+
+for(let i = 1; i <= 12; i++){
+  console.log(i) // value chiqadi
+}
+console.log(i)// i is not defide chiqadi yani i degan uzgaruvchi yuq deydi 
+// chunke let local uzgaruvchi edi u faqat for ichida ishlaydi fordan tashqarida ishlamaydi 
+
+// ==============
+
+for(var i=1; i <= 12; i++){
+  // console.log(i)
+}
+console.log(i)// bu yirda i ni valuesini chiqaradi 
+// chunke var global uzgaruvchi edi u uzi turgan functiondaham ishlay veradi
